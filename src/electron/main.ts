2159a163b1edc903332026caf2cc5b7199f3d67a
adbak <adbak.dev@gmail.com>
2025-09-25 23:03:33 +0200

    config

---
+++ src/electron/main.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+import { isDev } from "./utils.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({});
+  const dir = path.join(__dirname, "../dist-react/index.html");
+
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(dir);
+  }
+});
